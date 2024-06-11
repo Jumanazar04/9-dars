09d17d09d4e59dc772eb46ee30b4f2656130acae
Jumanazar04 <yusupovjumanazar004@gmail.com>
2024-06-11 07:56:07 +0500

    feat: create react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Categories from "./components/Categories";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GeneralLayout from "./components/GeneralLayout";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<GeneralLayout />}>
+        <Route index element={<Categories />} />
+      </Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
