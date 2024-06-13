2a800d9de2b579911e2459030a145d83c61be980
Jumanazar04 <yusupovjumanazar004@gmail.com>
2024-06-13 08:16:12 +0500

    add few changes

--- src/App.js
+++ src/App.js
@@ -2,28 +2,8 @@ import React from "react";
 import Categories from "./components/Categories";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GeneralLayout from "./components/GeneralLayout";
-import { Breadcrumb } from "antd";
+import ProductsPage from "./components/Products/ProductsPage";
 
-const MyBreadcrumb = () => {
-  const breadcrumbItems = [
-    {
-      title: 'Home',
-      href: '/', // Optionally add a link
-    },
-    {
-      title: 'List',
-      href: '/list', // Optionally add a link
-    },
-    {
-      title: 'App',
-      href: '/app', // Optionally add a link
-    },
-  ];
-
-  return (
-    <Breadcrumb items={breadcrumbItems} />
-  );
-};
 
 
 function App() {
@@ -32,7 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={<GeneralLayout />}>
         <Route index element={<Categories  />} />
-        <Route path="/" element={<MyBreadcrumb />} />
+        <Route path="products" element={<ProductsPage />} />
       </Route>
     </Routes>
     </BrowserRouter>
